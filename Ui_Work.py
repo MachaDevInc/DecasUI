642010f84b284ce1496b1ae9df43627222bd49c8
MachaDevInc <125951486+MachaDevInc@users.noreply.github.com>
2023-06-06 04:57:35 +0500

    Update Ui_Work.py

--- Ui_Work.py
+++ Ui_Work.py
@@ -166,5 +166,15 @@ class JobsMainWindow(QMainWindow):
     def retranslateUi(self):
         _translate = QCoreApplication.translate
         self.setWindowTitle(_translate("JobsMainWindow", "JobsMainWindow"))
-        self.label_2.setText(_translate(
-            "JobsMainWindow", "<html><head/><body><p align=\"center\"><span style=\" font-size:22pt; font-weight:600;\">RECENT JOBS</span></p></body></html>"))
+        self.label_2.setText(_translate("work", "<html><head/><body><p align=\"center\"><span style=\" font-size:22pt; font-weight:600;\">RECENT JOBS</span></p></body></html>"))
+        self.search_text.setHtml(_translate("work", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
+"<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
+"p, li { white-space: pre-wrap; }\n"
+"</style></head><body style=\" font-family:\'MS Shell Dlg 2\'; font-size:8pt; font-weight:400; font-style:normal;\">\n"
+"<p align=\"center\" style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><br /></p></body></html>"))
+        self.textBrowser_2.setHtml(_translate("work", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
+"<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
+"p, li { white-space: pre-wrap; }\n"
+"</style></head><body style=\" font-family:\'MS Shell Dlg 2\'; font-size:8pt; font-weight:400; font-style:normal;\">\n"
+"<p align=\"center\" style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:12pt; font-weight:600;\">Search</span></p></body></html>"))
+        

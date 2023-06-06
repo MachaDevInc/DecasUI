39cf8836a3b1058bfffe38d0d0440a93dd351ef0
MachaDevInc <125951486+MachaDevInc@users.noreply.github.com>
2023-06-06 05:16:22 +0500

    Update Ui_Work.py

--- Ui_Work.py
+++ Ui_Work.py
@@ -215,13 +215,13 @@ class JobsMainWindow(QMainWindow):
                 '<p align="center" style="-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;"><br /></p></body></html>',
             )
         )
-        self.textBrowser_2.setHtml(
+        self.textBrowser_2.setText(
             _translate(
-                "JobsMainWindow",
+                "work",
                 '<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0//EN" "http://www.w3.org/TR/REC-html40/strict.dtd">\n'
                 '<html><head><meta name="qrichtext" content="1" /><style type="text/css">\n'
                 "p, li { white-space: pre-wrap; }\n"
                 "</style></head><body style=\" font-family:'MS Shell Dlg 2'; font-size:8pt; font-weight:400; font-style:normal;\">\n"
-                '<p align="center" style=" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;"><span style=" font-size:12pt; font-weight:600;">Search</span></p></body></html>',
+                '<p align="center" style=" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;"><span style=" font-size:22pt; font-weight:600;">Search</span></p></body></html>',
             )
         )
